62040c55075f00f3d0bc63d625f9335339ea2256
JiaqianCAI <jiaqian.cai@student.uts.edu.au>
2025-08-30 08:42:38 +1000

    Rough Profile

--- frontend/src/SissiElement.tsx
+++ frontend/src/SissiElement.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import ProfileModal from "./ProfileModal"; 
+
 export default function SissiElement() {
-    return <div>
-        <p>hello Sissi!</p>
+  const [openProfile, setOpenProfile] = useState(false);
+
+  return (
+    <div>
+      <div className="card">
+        <button
+          className="create_button"
+          onClick={() => setOpenProfile(true)}
+        >
+          Profile
+        </button>
+      </div>
+
+      {openProfile && <ProfileModal close={() => setOpenProfile(false)} />}
     </div>
-}  
+  );
+}
