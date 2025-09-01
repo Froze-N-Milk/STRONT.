1ac78ac22f4d8f91eb83e3b643af8d2aadecbfbe
Froze-N-Milk <oscar.chevalier@gmail.com>
2025-09-01 23:41:00 +1000

    feat: add /api/sign-up endpoint

--- backend/api/auth.go
+++ backend/api/auth.go
@@ -7,9 +7,10 @@ import (
 	"net/http"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-type loginRequest struct {
+type request struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
@@ -19,10 +20,10 @@ type LoginHandler struct {
 }
 
 func (h *LoginHandler) ServeHTTP(ctx AppContext, w http.ResponseWriter, r *http.Request) {
-	login := loginRequest{}
+	login := request{}
 	err := json.NewDecoder(r.Body).Decode(&login) // Try decoding request to the login object
 	if err != nil {                               // If the decode failed, log the error and send status request back
-		slog.Error(err.Error())
+		slog.Error("invalid login request", "error", err, "request", r.Body)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -45,8 +46,48 @@ func (h *LoginHandler) ServeHTTP(ctx AppContext, w http.ResponseWriter, r *http.
 	}
 
 	// Return success if match
-	// TODO: Generate session tokens
 	err = setSessionTokenCookie(w, account.Email, h.JWTKey)
+	if err != nil {
+		slog.Error("unable to set session cookie", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Add("Location", "/account")
+	w.WriteHeader(http.StatusSeeOther)
+}
+
+type SignUpHandler struct {
+	JWTKey *[32]byte
+}
+
+func (h *SignUpHandler) ServeHTTP(ctx AppContext, w http.ResponseWriter, r *http.Request) {
+	signUp := request{}
+	err := json.NewDecoder(r.Body).Decode(&signUp)
+	if err != nil {
+		slog.Error("invalid sign-up request", "error", err, "request", r.Body)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Pull account matching email
+	salt, hash := CreateSaltAndHashPassword(signUp.Password)
+
+	err = gorm.G[Account](ctx.DB.Clauses(clause.OnConflict{DoNothing: true})).Create(r.Context(), &Account{
+		Email:        signUp.Email,
+		PasswordHash: hash[:],
+		PasswordSalt: salt[:],
+	})
+
+	if err != nil {
+		slog.Error("account already exists for email", "email", signUp.Email, "error", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// TODO: verify email
+
+	// set session cookie
+	err = setSessionTokenCookie(w, signUp.Email, h.JWTKey)
 	if err != nil {
 		slog.Error("unable to set session cookie", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
