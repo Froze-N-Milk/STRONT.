16093ff6d15c264a2e03a3849c418e4eeb9519ac
jupiterstreet <matthewhunyor@gmail.com>
2025-09-01 23:53:43 +1000

    style: deleted old styles, created base style and navbar for base page.

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,9 +1,22 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
 	component: () => <>
-		<Outlet />
+		<div id="pagecontent_container">
+			<div className="navbar_wrapper">
+				<div id="navbar_container">
+					<div className="stront">STRONT.</div>
+					<div className="navbar_items">
+						<Link className="navbar_button" to="/login">login</Link>
+						<Link className="navbar_button" to="/sign-up">sign up</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="pagebody_wrapper">
+			<Outlet />
+		</div>
 		<TanStackRouterDevtools />
 	</>,
 })
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,10 @@
 import './index.css'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute} from '@tanstack/react-router'
 
 function App() {
 
 	return <div>
-		Restaurant Bookin
 		{/* TODO: if logged in, change login to account? */}
-		<Link to="/login">login</Link>
-		<Link to="/sign-up">sign up</Link>
 	</div>
 }
 
