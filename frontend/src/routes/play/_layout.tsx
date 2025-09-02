4e4132e88220fe932f004cb64ab9f747bb4748a9
Jayden21221 <chanyoung.lee@student.uts.edu.au>
2025-09-03 00:43:48 +1000

    Update _layout.tsx

--- frontend/src/routes/play/_layout.tsx
+++ frontend/src/routes/play/_layout.tsx
@@ -2,7 +2,7 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 import PlayLayout from '../../components/play/_layout'
 
-// ⚠️ 포인트: 레이아웃 라우트 키는 '/play'
+// Layout routes key '/play'
 export const Route = createFileRoute('/play/_layout')({
   component: () => (
     <PlayLayout>
