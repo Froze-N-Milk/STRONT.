f72580c509d218e2441dc13bdfc514400f4ecde8
JiaqianCAI <jiaqian.cai@student.uts.edu.au>
2025-09-02 19:41:12 +1000

    Add account page link to home

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,6 +8,7 @@ function App() {
 		{/* TODO: if logged in, change login to account? */}
 		<Link to="/login">login</Link>
 		<Link to="/sign-up">sign up</Link>
+		<Link to="/account">account</Link>
 	</div>
 }
 
