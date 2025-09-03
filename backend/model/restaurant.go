bdd5943c5c8bb56e3a1e5166c9dc14fc63750327
James Chivers <raspberryjam.chivers@gmail.com>
2025-09-03 18:21:19 +1000

    feat: add Availability navigational property
    fix: remove incorrect not null tags from Description, LocationText, and LocationUrl

--- backend/model/restaurant.go
+++ backend/model/restaurant.go
@@ -3,13 +3,14 @@ package model
 import "github.com/google/uuid"
 
 type Restaurant struct {
-	ID             uuid.UUID `json:"id" gorm:"primaryKey;default:gen_random_uuid()"`
-	AccountID      uuid.UUID `gorm:"not null"`
-	AvailabilityID uuid.UUID `gorm:"not null"`
-	Name           string    `json:"name" gorm:"type:text;not null"`
-	Description    string    `json:"description" gorm:"type:text;not null"`
-	LocationText   string    `json:"location_test" gorm:"type:text;not null"`
-	LocationUrl    string    `json:"location_url" gorm:"type:text;not null"`
+	ID             uuid.UUID     `json:"id" gorm:"primaryKey;default:gen_random_uuid()"`
+	AccountID      uuid.UUID     `gorm:"not null"`
+	AvailabilityID uuid.UUID     `gorm:"not null"`
+	Name           string        `json:"name" gorm:"type:text;not null"`
+	Description    string        `json:"description" gorm:"type:text"`
+	LocationText   string        `json:"location_test" gorm:"type:text"`
+	LocationUrl    string        `json:"location_url" gorm:"type:text"`
+	Availability   *Availability `json:"availability" gorm:"foreignKey:AvailabilityID;references:ID"`
 }
 
 func (Restaurant) TableName() string {
