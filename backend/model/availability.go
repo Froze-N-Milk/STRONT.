52bcfbb3c54cb983077877c76f53dd36e91cd379
James Chivers <raspberryjam.chivers@gmail.com>
2025-09-03 16:33:07 +1000

    feat: model the availability table as a go struct

---
+++ backend/model/availability.go
@@ -0,0 +1,18 @@
+package model
+
+import "github.com/google/uuid"
+
+type Availability struct {
+	ID                uuid.UUID `json:"id" gorm:"primaryKey;default:gen_random_uuid()"`
+	MondayHourMask    int64     `json:"monday_hour_mask" gorm:"not null"`
+	TuesdayHourMask   int64     `json:"tuesday_hour_mask" gorm:"not null"`
+	WednesdayHourMask int64     `json:"wednesday_hour_mask" gorm:"not null"`
+	ThursdayHourMask  int64     `json:"thursday_hour_mask" gorm:"not null"`
+	FridayHourMask    int64     `json:"friday_hour_mask" gorm:"not null"`
+	SaturdayHourMask  int64     `json:"saturday_hour_mask" gorm:"not null"`
+	SundayHourMask    int64     `json:"sunday_hour_mask" gorm:"not null"`
+}
+
+func (Availability) TableName() string {
+	return "availability"
+}
