62027685761c27d14b966f763348e5779a24177c
James Chivers <raspberryjam.chivers@gmail.com>
2025-09-03 16:22:16 +1000

    fix: correct the gorm field tag syntax for id and email

--- backend/model/account.go
+++ backend/model/account.go
@@ -3,8 +3,8 @@ package model
 import "github.com/google/uuid"
 
 type Account struct {
-	ID           uuid.UUID `json:"id" gorm:"primary_key;default:gen_random_uuid()"`
-	Email        string    `json:"email" gorm:"index:idx_account_email,unique"`
+	ID           uuid.UUID `json:"id" gorm:"primaryKey;default:gen_random_uuid()"`
+	Email        string    `json:"email" gorm:"index:idx_account_email;unique"`
 	PasswordHash []byte    `gorm:"type:bytea"` // omitted json tag as we shouldn't be sending this to a client
 	PasswordSalt []byte    `gorm:"type:bytea"` // omitted json tag
 }
