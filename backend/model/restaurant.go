b84d98469b06ee43c245f141709e53ca52e016da
Oscar Chevalier <oscar.chevalier@gmail.com>
2025-09-26 13:48:50 +1000

    fix: add missing `FrontendMarkdown` field

--- backend/model/restaurant.go
+++ backend/model/restaurant.go
@@ -3,14 +3,15 @@ package model
 import "github.com/google/uuid"
 
 type Restaurant struct {
-	ID             uuid.UUID `gorm:"primaryKey;default:gen_random_uuid()"`
-	AccountID      uuid.UUID `gorm:"not null"`
-	AvailabilityID uuid.UUID `gorm:"not null"`
-	Name           string    `gorm:"type:text;not null"`
-	Description    string    `gorm:"type:text"`
-	LocationText   string    `gorm:"type:text"`
-	LocationUrl    string    `gorm:"type:text"`
-	Availability   *Availability
+	ID                uuid.UUID `gorm:"primaryKey;default:gen_random_uuid()"`
+	AccountID         uuid.UUID `gorm:"not null"`
+	AvailabilityID    uuid.UUID `gorm:"not null"`
+	Name              string    `gorm:"type:text;not null"`
+	Description       string    `gorm:"type:text"`
+	LocationText      string    `gorm:"type:text"`
+	LocationUrl       string    `gorm:"type:text"`
+	FrontpageMarkdown string    `gorm:"type:text"`
+	Availability      *Availability
 }
 
 func (Restaurant) TableName() string {
