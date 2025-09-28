d8e22727eaebbabed28c7a2de98c72a742289c96
James Chivers <raspberryjam.chivers@gmail.com>
2025-09-28 22:19:59 +1000

    feat: implement retrieving booking details by ID

--- backend/api/booking.go
+++ backend/api/booking.go
@@ -19,6 +19,77 @@ import (
 // TODO: Get list of booking history for a restaurant
 // TODO: Update booking details like attendance, restaurant notes, bill, paid
 
+//region Get Booking by ID
+
+// GetBookingByIDHandler retrieves the booking row from the database by its ID and returns a JSON object
+//
+// # No auth required
+//
+// GET /api/booking/{booking}
+//
+// expects: ID by URL
+//
+// returns:
+//
+//	{
+//		contact_id: string,
+//		restaurant_id: string,
+//		party_size: int,
+//		booking_date: string,
+//		time_slot: int,
+//		customer_notes: string
+//	}
+type GetBookingByIDHandler struct{}
+
+type getBookingByIDResponse struct {
+	ContactID     uuid.UUID `json:"contact_id"`
+	RestaurantID  uuid.UUID `json:"restaurant_id"`
+	PartySize     int       `json:"party_size"`
+	BookingDate   time.Time `json:"booking_date"`
+	TimeSlot      int       `json:"time_slot"`
+	CustomerNotes string    `json:"customer_notes"`
+}
+
+func (h *GetBookingByIDHandler) handle(ctx context.Context, db *gorm.DB, bookingId uuid.UUID) (getBookingByIDResponse, error) {
+	booking, err := gorm.G[model.Booking](db).Where("id = ?", bookingId).First(ctx)
+	if err != nil {
+		return getBookingByIDResponse{}, err
+	}
+
+	return getBookingByIDResponse{
+		ContactID:     booking.ContactID,
+		RestaurantID:  booking.RestaurantID,
+		PartySize:     booking.PartySize,
+		BookingDate:   booking.BookingDate,
+		TimeSlot:      booking.TimeSlot,
+		CustomerNotes: booking.CustomerNotes,
+	}, nil
+}
+
+func (h *GetBookingByIDHandler) ServeHTTP(ctx AppContext, w http.ResponseWriter, r *http.Request) {
+	bookingId, err := uuid.Parse(r.PathValue("booking"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response, err := h.handle(r.Context(), ctx.DB, bookingId)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(&response)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+//endregion
+
 //region Customer Online Booking
 
 // CreateOnlineBookingHandler creates booking rows in the database for bookings made online by customers
@@ -35,8 +106,9 @@ import (
 //		family_name: string,
 //		phone: string,
 //		email: string,
-//		start_time: string, (YYYY-MM-DDTHH:MM:ssZ)
-//		end_time: string (YYYY-MM-DDTHH:MM:ssZ)
+//		party_size: string,
+//		booking_date: string, (YYYY-MM-DDTHH:MM:ssZ)
+//		time_slot: int
 //	}
 type CreateOnlineBookingHandler struct{}
 
@@ -47,12 +119,12 @@ type createBookingRequest struct {
 	FamilyName   string    `json:"family_name"`
 	Phone        string    `json:"phone"`
 	Email        string    `json:"email"`
-	HeadCount    int       `json:"head_count"`
-	StartTime    time.Time `json:"start_time"`
-	EndTime      time.Time `json:"end_time"`
+	PartySize    int       `json:"party_size"`
+	BookingDate  time.Time `json:"booking_date"`
+	TimeSlot     int       `json:"time_slot"`
 }
 
-func (*CreateOnlineBookingHandler) handle(ctx context.Context, db *gorm.DB, request createBookingRequest) (model.Booking, error) {
+func (h *CreateOnlineBookingHandler) handle(ctx context.Context, db *gorm.DB, request createBookingRequest) (model.Booking, error) {
 	customerContact := model.CustomerContact{
 		GivenName:  request.GivenName,
 		FamilyName: request.FamilyName,
@@ -66,12 +138,13 @@ func (*CreateOnlineBookingHandler) handle(ctx context.Context, db *gorm.DB, requ
 	}
 
 	booking := model.Booking{
-		ContactID:      customerContact.ID,
-		RestaurantID:   request.RestaurantID,
-		StartTime:      request.StartTime,
-		EndTime:        request.EndTime,
-		CreationDate:   time.Now().UTC(),
-		CreationMethod: model.Online,
+		ContactID:       customerContact.ID,
+		RestaurantID:    request.RestaurantID,
+		PartySize:       request.PartySize,
+		BookingDate:     request.BookingDate,
+		TimeSlot:        request.TimeSlot,
+		CreationDate:    time.Now().UTC(),
+		CustomerCreated: true,
 	}
 	err = gorm.G[model.Booking](db).Create(ctx, &booking)
 
@@ -108,106 +181,6 @@ func (h *CreateOnlineBookingHandler) ServeHTTP(ctx AppContext, w http.ResponseWr
 
 //endregion
 
-//region Restaurant Booking Creation
-
-// CreateRestaurantBookingHandler creates a booking in the database for bookings made by restaurant staff for offline customers
-//
-// # Auth required
-//
-// expects:
-//
-//	{
-//		restaurant_id: uuid,
-//		given_name: string,
-//		family_name: string,
-//		phone: string,
-//		email: string,
-//		start_time: string, (YYYY-MM-DDTHH:MM:ssZ)
-//		end_time: string, (YYYY-MM-DDTHH:MM:ssZ)
-//		creation_method: string, (accepts: "online", "walk-in", "phone")
-//		preapproved: bool
-//	}
-type CreateRestaurantBookingHandler struct{}
-
-type createRestaurantBookingRequest struct {
-	RestaurantID   uuid.UUID `json:"restaurant_id"`
-	GivenName      string    `json:"given_name"`
-	FamilyName     string    `json:"family_name"`
-	Phone          string    `json:"phone"`
-	Email          string    `json:"email"`
-	HeadCount      int       `json:"head_count"`
-	StartTime      time.Time `json:"start_time"`
-	EndTime        time.Time `json:"end_time"`
-	CreationMethod string    `json:"creation_method"`
-	Preapproved    bool      `json:"preapproved"`
-}
-
-func (h *CreateRestaurantBookingHandler) handle(ctx context.Context, db *gorm.DB, user User, request createRestaurantBookingRequest) (model.Booking, error) {
-	customerContact := model.CustomerContact{
-		GivenName:  request.GivenName,
-		FamilyName: request.FamilyName,
-		Phone:      request.Phone,
-		Email:      request.Email,
-	}
-
-	err := gorm.G[model.CustomerContact](db).Create(ctx, &customerContact)
-
-	if err != nil {
-		return model.Booking{}, err
-	}
-
-	// Inserts new row only if the user account of this session owns the restaurant being requested
-	booking, err := gorm.G[model.Booking](db).Raw(`
-INSERT INTO booking (contact_id, restaurant_id, start_time, end_time, approved, head_count, creation_date, creation_method)
-SELECT $1, r.id, $3, $4, $5, $6, $7, $8
-FROM restaurant r
-JOIN account a ON r.account_id = a.id
-WHERE r.id = $2 AND a.email = $9
-RETURNING id`,
-		customerContact.ID,
-		request.RestaurantID,
-		request.StartTime,
-		request.EndTime,
-		request.Preapproved,
-		request.HeadCount,
-		time.Now().UTC(),
-		request.CreationMethod,
-		user.Email).Take(ctx)
-
-	if err != nil {
-		return model.Booking{}, err
-	}
-
-	return booking, nil
-}
-
-func (h *CreateRestaurantBookingHandler) ServeHTTP(ctx AuthedAppContext, w http.ResponseWriter, r *http.Request) {
-	request := createRestaurantBookingRequest{}
-	err := json.NewDecoder(r.Body).Decode(&request)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// Start GORM transaction
-	db := ctx.DB.Session(&gorm.Session{SkipDefaultTransaction: true}).Begin()
-	_, err = h.handle(r.Context(), db, ctx.User, request)
-
-	if err != nil {
-		db.Rollback()
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	// Transaction had no issues, so commit to database
-	db.Commit()
-
-	w.WriteHeader(http.StatusOK)
-	return
-}
-
-//endregion
-
 // region Booking Update
 
 // UpdateBookingHandler updates the specified booking row in the database
@@ -223,9 +196,8 @@ func (h *CreateRestaurantBookingHandler) ServeHTTP(ctx AuthedAppContext, w http.
 //	}
 type UpdateBookingHandler struct{}
 type updateBookingRequest struct {
-	StartTime time.Time `json:"start_time"`
-	EndTime   time.Time `json:"end_time"`
-	HeadCount int       `json:"head_count"`
+	TimeSlot  int `json:"time_slot"`
+	HeadCount int `json:"head_count"`
 }
 
 func (h *UpdateBookingHandler) handle(ctx context.Context, db *gorm.DB, request updateBookingRequest, bookingId uuid.UUID) error {
@@ -234,8 +206,7 @@ func (h *UpdateBookingHandler) handle(ctx context.Context, db *gorm.DB, request 
 		return err
 	}
 
-	booking.StartTime = request.StartTime
-	booking.EndTime = request.EndTime
+	booking.TimeSlot = request.TimeSlot
 	// booking.HeadCount = request.HeadCount TODO: Update when schema update is merged
 
 	db.Save(&booking)
--- backend/model/booking.go
+++ backend/model/booking.go
@@ -12,9 +12,9 @@ type Booking struct {
 	ID              uuid.UUID  `gorm:"primaryKey;default:gen_random_uuid()"`
 	ContactID       uuid.UUID  `gorm:"not null"`
 	RestaurantID    uuid.UUID  `gorm:"not null"`
-	PartySize       int32      `gorm:"not null"`
+	PartySize       int        `gorm:"not null"`
 	BookingDate     time.Time  `gorm:"type:date;not null"`
-	TimeSlot        int32      `gorm:"not null"`
+	TimeSlot        int        `gorm:"not null"`
 	CreationDate    time.Time  `gorm:"type:timestamptz;not null"`
 	CustomerCreated bool       `gorm:"default:false;not null"`
 	Attendance      Attendance `gorm:"type:text"`
