acdf53d3c756f9b9775276d0b01c64b72b3312a5
James Chivers <raspberryjam.chivers@gmail.com>
2025-10-02 00:22:05 +1000

    feat: implement db-integrated tests for CreateOnlineBooking

--- backend/api/booking_test.go
+++ backend/api/booking_test.go
@@ -1,31 +1,32 @@
 package api
 
 import (
-	"plange/backend/test"
+	"plange/backend/model"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func TestGetBookingByID(t *testing.T) {
-	test.WithTestDB(t, func(db *gorm.DB) {
-		seedData := test.SeedDB(db)
+	WithTestDB(t, func(db *gorm.DB) {
+		s := SeedDB(db)
 		handler := GetBookingByIDHandler{}
 
 		t.Run("Correct Booking ID", func(t *testing.T) {
 			expected := bookingRequest{
-				RestaurantID:  seedData.Restaurant.ID,
-				GivenName:     seedData.CustomerContact.GivenName,
-				FamilyName:    seedData.CustomerContact.FamilyName,
-				Phone:         seedData.CustomerContact.Phone,
-				Email:         seedData.CustomerContact.Email,
-				PartySize:     seedData.Booking.PartySize,
-				BookingDate:   seedData.Booking.BookingDate,
-				TimeSlot:      seedData.Booking.TimeSlot,
-				CustomerNotes: seedData.Booking.CustomerNotes,
+				RestaurantID:  s.Restaurant.ID,
+				GivenName:     s.CustomerContact.GivenName,
+				FamilyName:    s.CustomerContact.FamilyName,
+				Phone:         s.CustomerContact.Phone,
+				Email:         s.CustomerContact.Email,
+				PartySize:     s.Booking.PartySize,
+				BookingDate:   s.Booking.BookingDate,
+				TimeSlot:      s.Booking.TimeSlot,
+				CustomerNotes: s.Booking.CustomerNotes,
 			}
-			response, err := handler.handle(seedData.Ctx, db, seedData.Booking.ID)
+			response, err := handler.handle(s.Ctx, db, s.Booking.ID)
 			if err != nil {
 				t.Errorf("error running GetBookingByIDHandler.handle()")
 			}
@@ -34,9 +35,80 @@ func TestGetBookingByID(t *testing.T) {
 			}
 		})
 		t.Run("Invalid Booking ID", func(t *testing.T) {
-			response, err := handler.handle(seedData.Ctx, db, uuid.New())
+			response, err := handler.handle(s.Ctx, db, uuid.New())
 			if err == nil {
 				t.Errorf("Expected error, got %+v", response)
+			}
+		})
+	})
+}
+
+func TestCreateOnlineBooking(t *testing.T) {
+	WithTestDB(t, func(db *gorm.DB) {
+		s := SeedDB(db)
+		handler := CreateOnlineBookingHandler{}
+
+		t.Run("Valid Booking Data", func(t *testing.T) {
+			br := bookingRequest{
+				RestaurantID:  s.Restaurant.ID,
+				GivenName:     s.CustomerContact.GivenName,
+				FamilyName:    s.CustomerContact.FamilyName,
+				Phone:         s.CustomerContact.Phone,
+				Email:         s.CustomerContact.Email,
+				PartySize:     13,
+				BookingDate:   time.Date(2026, 1, 1, 0, 0, 0, 0, time.Local),
+				TimeSlot:      12,
+				CustomerNotes: "yay",
+			}
+			response, err := handler.handle(s.Ctx, db, br)
+			if err != nil {
+				t.Errorf("error running CreateOnlineBookingHandler.handle(): %v", err)
+			}
+
+			bookingRow, err := gorm.G[model.Booking](db).Where("id = ?", response.ID).First(ctx)
+			if err != nil {
+				t.Errorf("failed retrieving bookingRow from database: %v", err)
+			}
+
+			// TODO: Fix timezone issues between sending and retrieving from db
+			if !(bookingRow.ContactID == response.ContactID &&
+				bookingRow.RestaurantID == response.RestaurantID &&
+				bookingRow.PartySize == response.PartySize &&
+				bookingRow.TimeSlot == response.TimeSlot &&
+				bookingRow.CustomerCreated == response.CustomerCreated &&
+				bookingRow.Attendance == response.Attendance &&
+				bookingRow.CustomerNotes == response.CustomerNotes &&
+				bookingRow.RestaurantNotes == response.RestaurantNotes) {
+				t.Errorf("response does not match bookingRow returned from db query")
+			}
+
+			contactRow, err := gorm.G[model.CustomerContact](db).Where("id = ?", response.ContactID).First(ctx)
+			if err != nil {
+				t.Errorf("failed retrieving contactRow from database: %v", err)
+			}
+
+			if !(br.GivenName == contactRow.GivenName &&
+				br.FamilyName == contactRow.FamilyName &&
+				br.Phone == contactRow.Phone &&
+				br.Email == contactRow.Email) {
+				t.Errorf("response does not match contactRow returned from db query")
+			}
+		})
+		t.Run("Invalid Booking Data", func(t *testing.T) {
+			br := bookingRequest{
+				RestaurantID:  s.Restaurant.ID,
+				GivenName:     "",
+				FamilyName:    "zoinkeroonies",
+				Phone:         "jinkies",
+				Email:         "1234556666667778",
+				PartySize:     -4,
+				BookingDate:   time.Date(60000000, 1, 1, 0, 0, 0, 0, time.UTC),
+				TimeSlot:      17,
+				CustomerNotes: "yay",
+			}
+			response, err := handler.handle(s.Ctx, db, br)
+			if err == nil {
+				t.Errorf("expected error, got %v", response)
 			}
 		})
 	})
