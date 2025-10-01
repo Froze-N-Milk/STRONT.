154f60dcf503cc1938c4d9e25d4255cf896c7933
James Chivers <raspberryjam.chivers@gmail.com>
2025-10-02 00:21:42 +1000

    fix: add error checking to the db seeding

--- backend/api/harness_test.go
+++ backend/api/harness_test.go
@@ -1,22 +1,24 @@
-package test
+package api
 
 import (
 	"context"
 	"fmt"
 	"log"
 	"os"
-	"plange/backend/api"
 	"plange/backend/model"
+	"strings"
 	"testing"
 	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 var admin *gorm.DB
 var dbHost string
 var dbUser string
+var dbPort string
 var dbPassword string
 var ctx context.Context
 
@@ -44,17 +46,18 @@ func TestMain(m *testing.M) {
 	if dbPassword == "" {
 		log.Panic("DB_PASSWORD environment variable is empty. Cannot connect to testing database.")
 	}
+	dbPort = os.Getenv("DB_PORT")
 
-	adminDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=5432 sslmode=disable",
-		dbHost, dbUser, dbPassword)
+	adminDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable",
+		dbHost, dbUser, dbPassword, dbPort)
 
 	ctx = context.Background()
 
 	var err error // Set this so that admin can be set directly when calling gorm.Open
 
-	admin, err = gorm.Open(postgres.Open(adminDSN), &gorm.Config{})
+	admin, err = gorm.Open(postgres.Open(adminDSN))
 	if err != nil {
-		log.Panic("Unable to connect to testing database: %v", err)
+		log.Panicf("Unable to connect to testing database: %v", err)
 	}
 
 	code := m.Run()
@@ -62,13 +65,19 @@ func TestMain(m *testing.M) {
 }
 
 func SeedDB(db *gorm.DB) SeedData {
-	salt, hash := api.CreateSaltAndHashPassword("password")
+	result := gorm.WithResult()
+
+	salt, hash := CreateSaltAndHashPassword("password")
 	account := model.Account{
 		Email:        "test@test.com",
 		PasswordHash: hash[:],
 		PasswordSalt: salt[:],
 	}
-	_ = gorm.G[model.Account](db).Create(ctx, &account)
+	err := gorm.G[model.Account](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &account)
+
+	if err != nil {
+		log.Panicf("Failed to add account to db: %v", err)
+	}
 
 	const weekdayHours int64 = 0b0000000000000000000000000000000001111111111111111111111111000000
 	// bit mask for 10am-6pm
@@ -84,7 +93,11 @@ func SeedDB(db *gorm.DB) SeedData {
 		SundayHourMask:    weekendHours,
 	}
 
-	_ = gorm.G[model.Availability](db).Create(ctx, &availability)
+	err = gorm.G[model.Availability](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &availability)
+
+	if err != nil {
+		log.Panicf("Failed to add availability to db: %v", err)
+	}
 
 	occasion := model.Occasion{
 		AvailabilityID:  availability.ID,
@@ -93,7 +106,11 @@ func SeedDB(db *gorm.DB) SeedData {
 		YearlyRecurring: false,
 	}
 
-	_ = gorm.G[model.Occasion](db).Create(ctx, &occasion)
+	err = gorm.G[model.Occasion](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &occasion)
+
+	if err != nil {
+		log.Panicf("Failed to add occasion to db: %v", err)
+	}
 
 	restaurant := model.Restaurant{
 		AccountID:       account.ID,
@@ -106,7 +123,11 @@ func SeedDB(db *gorm.DB) SeedData {
 		BookingLength:   2,
 	}
 
-	_ = gorm.G[model.Restaurant](db).Create(ctx, &restaurant)
+	err = gorm.G[model.Restaurant](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &restaurant)
+
+	if err != nil {
+		log.Panicf("Failed to add restaurant to db: %v", err)
+	}
 
 	customerContact := model.CustomerContact{
 		GivenName:  "John",
@@ -115,7 +136,11 @@ func SeedDB(db *gorm.DB) SeedData {
 		Email:      "my@chungus.com",
 	}
 
-	_ = gorm.G[model.CustomerContact](db).Create(ctx, &customerContact)
+	err = gorm.G[model.CustomerContact](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &customerContact)
+
+	if err != nil {
+		log.Panicf("Failed to add customer contact to db: %v", err)
+	}
 
 	booking := model.Booking{
 		ContactID:       customerContact.ID,
@@ -130,7 +155,11 @@ func SeedDB(db *gorm.DB) SeedData {
 		RestaurantNotes: "yo this guy's weird",
 	}
 
-	_ = gorm.G[model.Booking](db).Create(ctx, &booking)
+	err = gorm.G[model.Booking](db.Clauses(clause.OnConflict{DoNothing: true}, result)).Create(ctx, &booking)
+
+	if err != nil {
+		log.Panicf("Failed to add booking to db: %v", err)
+	}
 
 	return SeedData{
 		Account:         account,
@@ -153,23 +182,25 @@ func SeedDB(db *gorm.DB) SeedData {
 //	}
 func WithTestDB(t *testing.T, testFunc func(db *gorm.DB)) {
 	// Get the test name to create a new DB with it
-	testDBName := fmt.Sprintf("%s_db", t.Name())
+	testDBName := strings.ToLower(fmt.Sprintf("%s_db", t.Name()))
 
 	// Create a new database from the template
-	err := admin.Exec(`CREATE DATABASE $1 TEMPLATE $2`,
+	admin.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDBName))
+	err := admin.Exec(fmt.Sprintf("CREATE DATABASE %s TEMPLATE %s",
 		testDBName,
-		"stront_template").Error
+		"stront_template")).Error
 
 	if err != nil {
 		t.Fatalf("Error creating test database: %v", err)
 	}
 
 	// Create the connection string for connecting to this new test db
-	testDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+	testDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		dbHost,
 		dbUser,
 		dbPassword,
-		testDBName)
+		testDBName,
+		dbPort)
 
 	// Establish the connection to the database
 	testDB, err := gorm.Open(postgres.Open(testDSN), &gorm.Config{})
@@ -179,7 +210,7 @@ func WithTestDB(t *testing.T, testFunc func(db *gorm.DB)) {
 
 	// Register a deferred cleanup job for when the test finishes running
 	t.Cleanup(func() {
-		admin.Exec(`DROP DATABASE IF EXISTS $1`, testDBName)
+		admin.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", testDBName))
 	})
 
 	testFunc(testDB)
