c3febf0ce630849d26df9250227f8249335ef203
Froze-N-Milk <oscar.chevalier@gmail.com>
2025-10-16 15:41:26 +1100

    feat(tests): add availability db tests

---
+++ backend/api/availability_test.go
@@ -0,0 +1,95 @@
+package api
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func TestGetAvailabilities(t *testing.T) {
+	t.Parallel()
+	WithTestDB(t, func(db *gorm.DB) {
+		s := SeedDB(db)
+
+		h := GetAvailabilitiesHandler{}
+
+		t.Run("Get Availabilities", func (t *testing.T) {
+			now := time.Now()
+			today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+			_, err := h.handle(s.Ctx, db, s.Restaurant.ID, today)
+
+			if err != nil {
+				t.Errorf("unexpected query error")
+			}
+		})
+		t.Run("Get Non-Existent Availabilities", func (t *testing.T) {
+			now := time.Now()
+			today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+			_, err := h.handle(s.Ctx, db, uuid.MustParse("ffffffff-ffff-ffff-ffff-ffffffffffff"), today)
+
+			if err == nil {
+				t.Errorf("expected query error")
+			}
+		})
+	})
+}
+
+func TestUpdateAvailabilities(t *testing.T) {
+	t.Parallel()
+	WithTestDB(t, func(db *gorm.DB) {
+		s := SeedDB(db)
+
+		h := UpdateAvailabilitiesHandler{}
+
+		t.Run("Update Availabilities", func (t *testing.T) {
+			err := h.handle(s.Ctx, db, s.Account.Email, updateRestaurantAvailabilitiesRequest{
+				ID:                s.Restaurant.ID,
+				MondayHourMask:    0,
+				TuesdayHourMask:   0,
+				WednesdayHourMask: 0,
+				ThursdayHourMask:  0,
+				FridayHourMask:    0,
+				SaturdayHourMask:  0,
+				SundayHourMask:    0,
+			})
+
+			if err != nil {
+				t.Errorf("unexpected query error: %#v", err)
+			}
+		})
+		t.Run("Update Non-Existent Availabilities", func (t *testing.T) {
+			err := h.handle(s.Ctx, db, s.Account.Email, updateRestaurantAvailabilitiesRequest{
+				ID:                uuid.MustParse("ffffffff-ffff-ffff-ffff-ffffffffffff"),
+				MondayHourMask:    0,
+				TuesdayHourMask:   0,
+				WednesdayHourMask: 0,
+				ThursdayHourMask:  0,
+				FridayHourMask:    0,
+				SaturdayHourMask:  0,
+				SundayHourMask:    0,
+			})
+
+			if err == nil {
+				t.Errorf("expected query error")
+			}
+		})
+		t.Run("Update Un-owned Availabilities", func (t *testing.T) {
+			err := h.handle(s.Ctx, db, "a", updateRestaurantAvailabilitiesRequest{
+				ID:                s.Availability.ID,
+				MondayHourMask:    0,
+				TuesdayHourMask:   0,
+				WednesdayHourMask: 0,
+				ThursdayHourMask:  0,
+				FridayHourMask:    0,
+				SaturdayHourMask:  0,
+				SundayHourMask:    0,
+			})
+
+			if err == nil {
+				t.Errorf("expected query error")
+			}
+		})
+	})
+}
